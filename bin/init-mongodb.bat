7b158b3b9e6d12d7d7a4ad4b0874f65a142afb8c
Sedlacek <martas.sedlacek@seznam.cz>
2021-12-08 01:12:41 +0100

    Updated project for local setup

--- bin/init-mongodb.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-REM Mongo DB initialisation
-REM Usage: .\bin\init-mongodb.bat --env=dev
-
-php bin/console doctrine:mongodb:schema:drop %*
-php bin/console doctrine:mongodb:schema:create %*
-
