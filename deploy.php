4cebcc3fc8e35bc8a093fbb439eb1d4f29db881c
Sedlacek <martas.sedlacek@seznam.cz>
2022-01-12 21:59:41 +0100

    Deployer test

---
+++ deploy.php
@@ -0,0 +1,57 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'Azimuth');
+
+// Project repository
+set('repository', 'git@github.com:sedlacek-martin/azimuth.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+
+set('default_timeout', 2000);
+set('http_user', 'azimutu');
+
+// Shared files/dirs between deploys
+add('shared_files', ['app/config/parameters.yml']);
+add('shared_dirs', ['var/log', 'var/sessions', 'web/media', 'web/uploads' ]);
+
+// Writable dirs by web server
+//add('writable_dirs', []);
+
+set('allow_anonymous_stats', false);
+
+
+// Hosts
+
+host('prod')
+    ->hostname(getenv('SERVER_HOSTNAME'))
+    ->user('azimutu')
+    ->port(22)
+    ->forwardAgent(true)
+    ->multiplexing(false)
+    ->set('deploy_path', '/usr/home/azimutu/public_html/gama')
+    ->set('keep_releases', 3);
+    
+// Tasks
+
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
