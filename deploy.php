a0571421db89ba52fe743da5699eb0f08938832b
Sedlacek <martas.sedlacek@seznam.cz>
2022-01-12 22:52:10 +0100

    Added tasks to deployer

--- deploy.php
+++ deploy.php
@@ -1,7 +1,12 @@
 <?php
 namespace Deployer;
 
+use function Clue\StreamFilter\fun;
+
 require 'recipe/common.php';
+
+$env = 'prod2';
+$hostname = 'www192.your-server.de';
 
 // Project name
 set('application', 'Azimuth');
@@ -28,13 +33,12 @@ set('allow_anonymous_stats', false);
 // Hosts
 
 host('prod')
-    ->hostname('www192.your-server.de')
-//    ->hostname(getenv('SERVER_HOSTNAME'))
+    ->hostname($hostname)
     ->user('azimutu')
     ->port(222)
     ->forwardAgent(true)
     ->multiplexing(false)
-    ->set('deploy_path', '/usr/home/azimutu/public_html/gama')
+    ->set('deploy_path', "/usr/home/azimutu/public_html/{$env}")
     ->set('keep_releases', 3);
     
 // Tasks
@@ -45,6 +49,8 @@ task('deploy', [
     'deploy:lock',
     'deploy:release',
     'deploy:update_code',
+    'composer',
+    'assets',
     'deploy:shared',
     'deploy:writable',
     'deploy:symlink',
@@ -52,6 +58,20 @@ task('deploy', [
     'cleanup',
     'success'
 ]);
+
+task('composer', function () use ($env) {
+    run("composer prod --no-interaction -d /usr/home/azimutu/public_html/{$env}/release");
+    run("composer dump-autoload --optimize --classmap-authoritative --no-interaction -d /usr/home/azimutu/public_html/{$env}/release");
+});
+
+task('migration', function () use ($env) {
+    run("/usr/home/azimutu/public_html/{$env}/release/bin/console doctrine:migrations:migrate -n");
+});
+
+task('assets', function () use ($env) {
+    run("/usr/home/azimutu/public_html/{$env}/release/bin/console assets:install --symlink");
+    run("/usr/home/azimutu/public_html/{$env}/release/bin/console assetic:dump");
+});
 
 // If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
