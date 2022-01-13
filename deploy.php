ca341b8c11627d9eb1218394cad5b7b9077dd077
Sedlacek <martas.sedlacek@seznam.cz>
2022-01-13 18:38:28 +0100

    Fix deploy composer

--- deploy.php
+++ deploy.php
@@ -60,6 +60,7 @@ task('deploy', [
 ]);
 
 task('composer', function () use ($env) {
+    cd('{{release_path}}');
     run("composer prod --no-interaction -d /usr/home/azimutu/public_html/{$env}/release");
     run("composer dump-autoload --optimize --classmap-authoritative --no-interaction -d /usr/home/azimutu/public_html/{$env}/release");
 });
